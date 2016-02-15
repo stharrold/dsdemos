d6060e577f0a2f98e7013d9c23e63e7eef7906de
stharrold <samuel.harrold@gmail.com>
2016-02-15 23:05:50 +0000

    ml.py: added calc_score_pvalue, ref #9

--- dsdemos/ml.py
+++ dsdemos/ml.py
@@ -12,7 +12,9 @@ import warnings
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy
 import seaborn as sns
+import sklearn.cross_validation as sk_cv
 # Import local packages.
 import dsdemos.utils as utils
 
@@ -22,9 +24,10 @@ def calc_feature_importances(
     df_features:pd.DataFrame, ds_target:pd.Series, ds_weight:pd.Series=None, 
     size_sub:int=None, replace:bool=True, dists:list=None, 
     show_progress:bool=False, show_plot:bool=False) -> pd.DataFrame:
-    r"""Calculate feature importances and compare to random added features.
+    r"""Calculate feature importances and compare to random added features
+    for weighted data sets.
     
-    Parameters:
+    Args:
         estimator: Estimator model from `sklearn` with attributes `fit`
             and, after running `fit`, also `feature_importances_`.
             See 'Raises', 'Notes'.
@@ -36,7 +39,8 @@ def calc_feature_importances(
             weights. Format: rows=records, col=weight.
             Default: `None`, then all set to 1.
         size_sub (int, optional, None): Number of records in subset for
-            selecting features. Default: `None`, then all records are used.
+            selecting features.
+            Default: `None`, then min(10K, all) records are used.
             The larger `size_sub` the more features that will become
             significant. See 'Raises'.
         replace (bool, optional, True): Sample records with replacement
@@ -87,7 +91,7 @@ def calc_feature_importances(
     df_ftrs_rnd = df_features.copy()
     size_data = len(df_features)
     if size_sub is None:
-        size_sub = size_data
+        size_sub = min(int(1e4), size_data)
     dists = [
         'np.random.laplace(loc=0.0, scale=1.0, size=size_data)',
         'np.random.logistic(loc=0.0, scale=1.0, size=size_data)',
@@ -138,3 +142,179 @@ def calc_feature_importances(
         plt.ylabel("Feature column name")
         plt.show()
     return df_importances
+
+
+def calc_score_pvalue(
+    estimator,
+    df_features:pd.DataFrame, ds_target:pd.Series, ds_weight:pd.Series=None,
+    n_iter:int=20, frac_true:float=0.2, size_sub:int=None, frac_test:float=0.2,
+    replace:bool=True, show_progress:bool=False, show_plot:bool=False) -> float:
+    r"""Calculate the p-value of the scored predictions for weighted data sets.
+    
+    Args:
+        estimator: Estimator model from `sklearn` with attributes `fit`
+            and, after running `fit`, also `score`. See 'Raises', 'Notes'.
+        df_features (pandas.DataFrame): Data frame of feature values.
+            Format: rows=records, cols=features.
+        ds_target (pandas.Series): Data series of target values.
+            Format: rows=records, col=target.
+        ds_weight (pandas.Series, optional, None): Data series of record
+            weights. Format: rows=records, col=weight.
+            Default: `None`, then all set to 1.
+        n_iter (int, optional, 20): Number of iterations for calculating scores.
+        frac_true (float, optional, 0.2): Proportion of `n_iter` for which the
+            target values are not shuffled.
+        size_sub (int, optional, None): Number of records in subset for
+            cross-validating scores. Only enough records need to be used to
+            approximate the variance in the data.
+            Default: `None`, then min(10K, all) records are used.
+            See 'Notes', 'Raises'.
+        frac_test (float, optional, 0.2): Proportion of `size_sub` for which to
+            test the predicted target values and calculate each score.
+        replace (bool, optional, True): Sample records with replacement
+            (bootstrap sampling). Default: `True`.
+            Use `replace=True` to reduce overfitting the scores.
+        show_progress (bool, optional, False): Whether or not to print status.
+        show_plot (bool, optional, False): Whether or not to show summary plot
+            of score significance.
+    
+    Returns:
+        pvalue
+    
+    Raises:
+        ValueError:
+            * Raised if not `hasattr(estimator, 'fit')`
+        RuntimeWarning:
+            * Raised if not `size_sub <= len(df_features)`.
+    
+    See Also:
+        sklearn.cross_validation.train_test_split
+    
+    Notes:
+        * The significance test is calculated by sampling the differences
+            between the score means then shuffling the labels for whether or
+            not the target values were themselves shuffled.
+    
+    References:
+        [^kfold]: http://scikit-learn.org/stable/modules/generated/
+            sklearn.cross_validation.KFold.html
+
+    """
+    # TODO: Replace show_progress and warnings.warn with logger.[debug,warn]
+    #     https://github.com/stharrold/stharrold.github.io/issues/58
+    # Check arguments.
+    if not hasattr(estimator, 'fit'):
+        raise ValueError(
+            ("`estimator` must have the attribute 'fit'.\n" +
+             "Required: hasattr(estimator, 'fit')"))
+    if (size_sub is not None) and not (size_sub <= len(df_features)):
+        warnings.warn(
+            ("The number of records in the subset for calculating feature\n" +
+             "importances is larger than the number of records in the data.\n" +
+             "Suggested: size_sub <= len(df_features)\n" +
+             "Given: {lhs} <= {rhs}").format(
+                 lhs=size_sub, rhs=len(df_features)),
+             RuntimeWarning)
+    size_data = len(df_features)
+    if size_sub is None:
+        size_sub = min(int(1e4), size_data)
+    # Score with/without shuffling the target values.
+    inum_score_isshf = dict()
+    imod = int(n_iter*frac_true)
+    if show_progress:
+        print("Progress:", end=' ')
+    for inum in range(0, n_iter):
+        inum_score_isshf[inum] = dict()
+        idxs_sub = np.random.choice(a=size_data, size=size_sub, replace=replace)
+        if inum % imod == 0:
+            # 1 out of imod times without shuffling target values.
+            inum_score_isshf[inum]['is_shf'] = False
+            trg_vals = ds_target.values[idxs_sub]
+        else:
+            # Otherwise with randomly permuted target values.
+            inum_score_isshf[inum]['is_shf'] = True
+            trg_vals = np.random.permutation(ds_target.values[idxs_sub])
+        if ds_weight is None:
+            (ftrs_train, ftrs_test,
+             trg_train, trg_test) = sk_cv.train_test_split(
+                df_features.values[idxs_sub], trg_vals, test_size=frac_test)
+            estimator.fit(X=ftrs_train, y=trg_train)
+            inum_score_isshf[inum]['score'] = estimator.score(
+                X=ftrs_test, y=trg_test)
+        else:
+            (ftrs_train, ftrs_test,
+             trg_train, trg_test,
+             pwt_train, pwt_test) = sk_cv.train_test_split(
+                df_features.values[idxs_sub], trg_vals,
+                ds_weight.values[idxs_sub], test_size=frac_test)
+            estimator.fit(X=ftrs_train, y=trg_train, sample_weight=pwt_train)
+            inum_score_isshf[inum]['score'] = estimator.score(
+                X=ftrs_test, y=trg_test, sample_weight=pwt_test)
+        if show_progress:
+            print("{frac:.0%}".format(frac=(inum+1)/n_iter), end=' ')
+    if show_progress:
+        print('\n')
+    df_scores = pd.DataFrame.from_dict(data=inum_score_isshf, orient='index')
+    # Plot the distributions of model scores with/without
+    # shuffling the target values.
+    if show_plot:
+        sns.distplot(
+            df_scores.loc[df_scores['is_shf'], 'score'],
+            hist=True, kde=True, norm_hist=True, color=sns.color_palette()[0],
+            label='scores with shuffled target values')
+        sns.distplot(
+            df_scores.loc[np.logical_not(df_scores['is_shf']), 'score'],
+            hist=True, kde=True, norm_hist=True, color=sns.color_palette()[1],
+            label='scores with actual target values')
+        plt.title(
+            "Probability density functions of model scores\n" +
+            "by whether or not target values were permuted")
+        plt.xlabel("Model score")
+        plt.ylabel("Probability density")
+        plt.legend(loc='upper left')
+        plt.show()
+    # Calculate the distribution of differences in score means with/without
+    # shuffling the target values.
+    # Distribution of randomized differences in score means:
+    rnd_mean_score_diffs = list()
+    for _ in range(int(1e4)):
+        tfmask_shf = np.random.permutation(df_scores['is_shf'].values)
+        tfmask_notshf = np.logical_not(tfmask_shf)
+        rnd_mean_score_diffs.append(
+            df_scores.loc[tfmask_notshf, 'score'].mean()
+            - df_scores.loc[tfmask_shf, 'score'].mean())
+    # Distribution of actual differences in score means:
+    tfmask_shf = df_scores['is_shf'].values
+    tfmask_notshf = np.logical_not(tfmask_shf)
+    mean_score_diff = (
+        df_scores.loc[tfmask_notshf, 'score'].mean()
+        - df_scores.loc[tfmask_shf, 'score'].mean())
+    # Plot the distribution of differences in score means.
+    if show_plot:
+        sns.distplot(rnd_mean_score_diffs, hist=True, kde=True, norm_hist=True,
+            color=sns.color_palette()[0],
+            label=(
+                'mean score differences assuming\n' +
+                'no distinction between shuffled/unshuffled'))
+        plt.axvline(mean_score_diff,
+            color=sns.color_palette()[1], label='actual mean score difference')
+        plt.title(
+            "Differences between mean model scores\n" +
+            "by whether or not target values were actually shuffled")
+        plt.xlabel("Model score difference")
+        plt.ylabel("Probability density")
+        plt.legend(loc='upper left')
+        plt.show()
+    # Return the p-value and describe the statistical significance.
+    pvalue = 100.0 - scipy.stats.percentileofscore(
+        a=rnd_mean_score_diffs, score=mean_score_diff, kind='mean')
+    if show_plot:
+        print(
+            ("Null hypothesis: There is no distinction in the differences\n" +
+             "between the mean model scores whether or not the target\n" +
+             "values have been shuffled.\n" +
+             "Outcome: Assuming the null hypothesis, the probability of\n" +
+             "obtaining a difference between the mean model scores at least\n" +
+             "as great as {diff:.2f} is {pvalue:.1f}%.").format(
+                 diff=mean_score_diff, pvalue=pvalue))
+    return pvalue
--- tests/test_ml.py
+++ tests/test_ml.py
@@ -17,3 +17,8 @@ import dsdemos.ml as ml
 # TODO: def_test_calc_feature_importances
 #     Use iris data set, set random seed to 0
 #     Test with/without weights
+# TODO: def_test_calc_score_pvalue
+#     Test with/without weights
+#     Compare to
+#     http://scikit-learn.org/stable/auto_examples/feature_selection/
+#     plot_permutation_test_for_classification.html
